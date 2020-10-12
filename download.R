4bf196a74c4f646904513a1f34b999c4cac936bd
rqthomas <rqthomas@vt.edu>
2020-10-12 17:36:55 +0000

    updated on server

--- download.R
+++ download.R
@@ -10,15 +10,13 @@ start_date = NA
 
 # Beetle
 # DP1.10022.001
-print("Downloading: DP1.10022.001")
-neonstore::neon_download(product="DP1.10022.001", type = "expanded", start_date = start_date, .token = Sys.getenv("NEON_TOKEN"))
-neonstore::neon_store(product="DP1.10022.001")
+neonstore::neon_download(product="DP1.10022.001")
 
 # Ticks
 # DP1.10093.001
 print("Downloading: DP1.10093.001")
 neonstore::neon_download(product ="DP1.10093.001", type = "expanded", start_date = start_date, .token = Sys.getenv("NEON_TOKEN"))
-neonstore::neon_store(product ="DP1.10093.001")
+#neonstore::neon_store(product ="DP1.10093.001")
 
 # Terrestrial
 #DP4.00200.001 & DP1.00094.001
@@ -27,7 +25,7 @@ print("Downloading: DP4.00200.001")
 neonstore::neon_download(product = "DP4.00200.001", site = sites, type = "basic", start_date = start_date, .token = Sys.getenv("NEON_TOKEN"))
 print("Downloading: DP1.00094.001")
 neonstore::neon_download(product = "DP1.00094.001", site = sites, type = "basic", start_date = start_date, .token = Sys.getenv("NEON_TOKEN"))
-neonstore::neon_store(table = "SWS_30_minute")
+
 
 # Aquatic
 #DP1.20053.001
@@ -38,10 +36,8 @@ neonstore::neon_download(product="DP1.20053.001",site = sites, type = "basic", s
 #neonstore::neon_store(product="DP1.20053.001")
 print("Downloading: DP1.20288.001")
 neonstore::neon_download(product="DP1.20288.001",site = sites, type = "basic", start_date = start_date, .token = Sys.getenv("NEON_TOKEN"))
-neonstore::neon_store(table = "waq_instantaneous")
 print("Downloading: DP1.20264.001")
 neonstore::neon_download("DP1.20264.001", site =  sites, type = "basic", start_date = start_date, .token = Sys.getenv("NEON_TOKEN"))
-neonstore::neon_store(table = "TSD_30_min")
 
 #Phenology
 
